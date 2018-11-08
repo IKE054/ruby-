7ee5150219af46c087f6c3efb3b9c913c5f49ce2
Mikoshi54 <Mikoshi54@Wacka54noMacBook-puro.local>
2018-11-08 15:48:59 +0900

    finish ruby

---
+++ lesson4.rb
@@ -0,0 +1,3 @@
+name = "koshi ikegami"
+
+puts name
---
+++ lesson7.rb
@@ -0,0 +1,34 @@
+# quiestion1
+puts "計算を始めます"
+puts "2つの値を入力してください"
+
+a = gets.to_i
+b = gets.to_i
+
+puts "計算結果を出力します"
+puts "a*b="+" #{a+b}"
+puts "計算を終了します"
+
+# question2
+puts "計算を始めます"
+puts "何回繰り返しますか？"
+
+times = gets.to_i
+
+for i in 1..times do
+
+puts "#{i}回目の計算"
+puts "2つの値を入力してください"
+
+a = gets.to_i
+b = gets.to_i
+
+puts "計算結果を出力します"
+puts "a+b= #{a+b}"
+puts "a-b= #{a-b}"
+puts "a*b= #{a*b}"
+puts "a/b= #{a/b}"
+
+end
+puts "計算を終了します"
+
